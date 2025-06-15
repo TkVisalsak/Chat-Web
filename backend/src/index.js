dfbddf8d45e1d5749cc04eaf2f96223ff5ff180b
TkVisalsak <visalsak2005@gmail.com>
2025-06-15 22:01:00 +0700

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -34,8 +34,7 @@ app.use("/api/messages", messageRoutes);
 
 // Connect to DB and start the server
 connectDB();
-const HOST = '127.0.0.1'; // match this to your frontend host
-
+const HOST = process.env.PORT ? "0.0.0.0" : "127.0.0.1";
 
 
 server.listen(PORT, HOST, () => {
